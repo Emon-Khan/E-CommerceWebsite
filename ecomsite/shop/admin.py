4ffe07c7db84041a0c9a0cd4b0c5e19728c1d98e
Emon-Khan <rkemon94@gmail.com>
2024-04-15 17:33:54 +0600

    Basic config of Checkout page has been done

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Order
 # Register your models here.
 
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ('title', 'discounted_price', 'category')
 
 admin.site.register(Products, ProductsAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_order = ('name', 'email', 'address', 'city', 'state')
+
+admin.site.register(Order, OrderAdmin)
